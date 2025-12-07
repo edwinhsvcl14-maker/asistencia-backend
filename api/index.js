e56502d0d5f3cd447ad8b9b28457121dc7e826eb
edwinhsvcl14-maker <edwinhsvcl14@gmail.com>
2025-12-07 18:09:32 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const empleadosDB = [
 const asistenciaDB = []; 
 
 // Middleware
-app.use(cors());
+app.use(cors()); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
